c268b7f3bc216c28f33c4528e2a938a2d10d03af
Diego Willian <diego2102.ds@gmail.com>
2020-09-10 21:32:08 -0300

    APIBoletim finalizado

---
+++ Context/BoletimContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBoletim.Context
+{
+    public class BoletimContext
+    {
+        SqlConnection con = new SqlConnection();
+
+        public BoletimContext()
+        {
+            con.ConnectionString = @"Data Source=DESKTOP-I0J8247\SQLEXPRESS;Initial Catalog=boletim;Integrated Security=True";
+        }
+
+        public SqlConnection Conectar()
+        {
+            if (con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+
+            return con;
+        }
+
+        public void Desconectar()
+        {
+            if (con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Close();
+            }
+        }
+
+
+
+    }
+}
